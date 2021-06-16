e69764b386c95dd219a66f16503ca94416526c1b
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-06-16 20:40:59 -0300

    FUCKING INTEX

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
-import { makeServer } from '../../services/mirage/intex';
+import { makeServer } from '../../services/mirage';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
