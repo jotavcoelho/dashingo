01d76685ddff66f2933502c9d61ad44a35d1f0c3
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-07-05 21:37:34 -0300

    listing seeded users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -28,8 +28,23 @@ export default function UserList() {
   const { data, isLoading, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json();
+    
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('en-US', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }),
+      }
+    });
 
-    return data;
+
+
+    return users;
   });
 
   const isWide = useBreakpointValue({
@@ -86,75 +101,33 @@ export default function UserList() {
                 </Tr>
               </Thead>
               <Tbody>
-                <Tr>
-                  <Td px={["4", "4", "8"]}>
-                  <Checkbox colorScheme="pink"/>
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Diego Fernandes</Text>
-                      <Text fontSize="sm" color="gray.300">emaildodiego.com@www/br</Text>
-                    </Box>
-                  </Td>
-                  {isWide && <Td>April 4th, 2021</Td>}
-                  {isWide && <Td>
-                    <Button
-                      as="a" 
-                      size="sm" 
-                      fontSize="sm" 
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                    >
-                      { isWide ? 'Edit' : '' }
-                    </Button>
-                  </Td>}
-                </Tr>
-                <Tr>
-                  <Td px={["4", "4", "8"]}>
-                  <Checkbox colorScheme="pink"/>
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Diego Fernandes</Text>
-                      <Text fontSize="sm" color="gray.300">emaildodiego.com@www/br</Text>
-                    </Box>
-                  </Td>
-                  {isWide && <Td>April 4th, 2021</Td>}
-                  {isWide && <Td>
-                    <Button
-                      as="a" 
-                      size="sm" 
-                      fontSize="sm" 
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                    >
-                      { isWide ? 'Edit' : '' }
-                    </Button>
-                  </Td>}
-                </Tr>
-                <Tr>
-                  <Td px={["4", "4", "8"]}>
-                  <Checkbox colorScheme="pink"/>
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Diego Fernandes</Text>
-                      <Text fontSize="sm" color="gray.300">emaildodiego.com@www/br</Text>
-                    </Box>
-                  </Td>
-                  {isWide && <Td>April 4th, 2021</Td>}
-                  {isWide && <Td>
-                    <Button
-                      as="a" 
-                      size="sm" 
-                      fontSize="sm" 
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
-                    >
-                      { isWide ? 'Edit' : '' }
-                    </Button>
-                  </Td>}
-                </Tr>
+                {data.map(user => {
+                  return (
+                    <Tr key={user.id}>
+                      <Td px={["4", "4", "8"]}>
+                        <Checkbox colorScheme="pink"/>
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                        </Box>
+                      </Td>
+                      {isWide && <Td>{user.createdAt}</Td>}
+                      {isWide && <Td>
+                        <Button
+                          as="a" 
+                          size="sm" 
+                          fontSize="sm" 
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                        >
+                          { isWide ? 'Edit' : '' }
+                        </Button>
+                      </Td>}
+                    </Tr>
+                  )
+                })}
               </Tbody>
             </Table>
 
