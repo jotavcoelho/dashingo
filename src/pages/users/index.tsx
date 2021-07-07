d7f9d695de1b90a18169f68eb7c00106f6746f87
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-07-06 21:01:01 -0300

    did some code organization, created useUsers hook and separated some stuff

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -20,32 +20,14 @@ import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { useQuery } from 'react-query';
 import { api } from "../../../services/api";
+import { useUsers } from "../../../services/hooks/useUsers";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get('users')
-    
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('en-US', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        }),
-      }
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5 // 5 seconds
-  });
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWide = useBreakpointValue({
     base: false,
