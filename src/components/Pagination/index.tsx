270fb3cc2e4cfaafe726dceb60e0e8eb9e351810
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-07-07 20:35:45 -0300

    finished interface implementation of pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack, Box } from "@chakra-ui/react";
+import { Button, Stack, Box, Text } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
 interface PaginationProps {
@@ -46,7 +46,16 @@ export function Pagination({
         <strong>0</strong> - <strong>10</strong> of <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        {/*  */}
+
+        {currentPage > (1 + siblingsCount) && (
+          <>
+            <PaginationItem number={1} />
+            { currentPage > (2 + siblingsCount) && (
+              <Text color="gray.300" width="8" textAlign="center">...</Text>
+            )}
+          </>
+        )}
+
         {previousPages.length > 0 && previousPages.map(page => {
           return <PaginationItem key={page} number={page} />
         })}
@@ -56,7 +65,16 @@ export function Pagination({
         {nextPages.length > 0 && nextPages.map(page => {
           return <PaginationItem key={page} number={page} />
         })}
-        {/*  */}
+
+        {(currentPage + siblingsCount) < lastPage && (
+          <>
+            { (currentPage + 1 + siblingsCount) < lastPage && (
+              <Text color="gray.300" width="8" textAlign="center">...</Text>
+            )}
+            <PaginationItem number={lastPage} />
+          </>
+
+        )}
       </Stack>
     </Stack>
   ); 
