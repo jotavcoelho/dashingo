58aa2d5b8d30f8d58f080fea096b7b3f653d8bf3
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-07-10 18:50:37 -0300

    now using mutations to create users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -44,7 +44,7 @@ export default function UserList() {
     .then(data => console.log(data));
   }, []);
 
-  async function handlePrefetchUser(userId: number) {
+  async function handlePrefetchUser(userId: string) {
     await queryClient.prefetchQuery(['user', userId], async () => {
       const response = await api.get(`users/${userId}`);
 
